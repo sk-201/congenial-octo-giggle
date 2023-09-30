a34f40875fa15e74d52180413d9c2ba1346430eb
Mohd Salar Khan <mohdsalarkhan@Mohd-Salar-Khans-MacBook-Pro.local>
2023-09-30 21:43:05 +0530

    minor change

--- frontend/src/components/ProjectGrid/index.jsx
+++ frontend/src/components/ProjectGrid/index.jsx
@@ -115,69 +115,83 @@ export default function ProjectGrid() {
         ></Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           <Grid container spacing={4}>
-            {projects.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    cursor: "pointer",
-                  }}
-                >
-                  <ButtonBase onClick={() => toggleDrawer(card?._id)}>
-                    <CardContent sx={{ flexGrow: 1 }}>
-                      <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="h1"
-                        bgcolor="#5d6acf"
-                        borderRadius={10}
-                        color="#f1f1f1"
-                        fontWeight={600}
-                      >
-                        Title
-                      </Typography>
-                      <Typography variant="subtitle2" fontWeight={400}>
-                        {card?.Project?.Title}
-                      </Typography>
-                      <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="h1"
-                        bgcolor="#c27e25"
-                        borderRadius={10}
-                        color="#f1f1f1"
-                        fontWeight={600}
-                      >
-                        Technologies
-                      </Typography>
-                      <Typography variant="button" fontWeight={200}>
-                        {card?.Project?.Technologies}
-                      </Typography>
-                      <Typography
-                        gutterBottom
-                        variant="subtitle1"
-                        component="h1"
-                        bgcolor="#56ba80"
-                        borderRadius={10}
-                        color="#f1f1f1"
-                        fontWeight={600}
-                      >
-                        Other Information
-                      </Typography>
-                      <Typography
-                        variant="subtitle2"
-                        fontWeight={600}
-                        color="#d13414"
-                      >
-                        {card?.Other_Information?.Availability}
-                      </Typography>
-                    </CardContent>
-                  </ButtonBase>
-                </Card>
-              </Grid>
-            ))}
+            {projects.length === 0 ? (
+              <Typography
+                variant="subtitle1"
+                fontWeight={600}
+                fontSize={45}
+                textAlign="center"
+                flexDirection="row"
+                alignItems="center"
+                width={"100vw"}
+              >
+                No Records found
+              </Typography>
+            ) : (
+              projects.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <ButtonBase onClick={() => toggleDrawer(card?._id)}>
+                      <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography
+                          gutterBottom
+                          variant="subtitle1"
+                          component="h1"
+                          bgcolor="#5d6acf"
+                          borderRadius={10}
+                          color="#f1f1f1"
+                          fontWeight={600}
+                        >
+                          Title
+                        </Typography>
+                        <Typography variant="subtitle2" fontWeight={400}>
+                          {card?.Project?.Title}
+                        </Typography>
+                        <Typography
+                          gutterBottom
+                          variant="subtitle1"
+                          component="h1"
+                          bgcolor="#c27e25"
+                          borderRadius={10}
+                          color="#f1f1f1"
+                          fontWeight={600}
+                        >
+                          Technologies
+                        </Typography>
+                        <Typography variant="button" fontWeight={200}>
+                          {card?.Project?.Technologies}
+                        </Typography>
+                        <Typography
+                          gutterBottom
+                          variant="subtitle1"
+                          component="h1"
+                          bgcolor="#56ba80"
+                          borderRadius={10}
+                          color="#f1f1f1"
+                          fontWeight={600}
+                        >
+                          Other Information
+                        </Typography>
+                        <Typography
+                          variant="subtitle2"
+                          fontWeight={600}
+                          color="#d13414"
+                        >
+                          {card?.Other_Information?.Availability}
+                        </Typography>
+                      </CardContent>
+                    </ButtonBase>
+                  </Card>
+                </Grid>
+              ))
+            )}
           </Grid>
         </Container>
       </main>
